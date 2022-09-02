f3120641f195aed01be4f9e735fb5512a9c4e730
Arijit Sehanobish <arijitthegame@gmail.com>
2022-09-01 23:28:30 -0400

    remove duplicate files

--- src/ega/util/utils.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def rescale_data(mesh, scale):
-    """Function to rescale the meshes. The meshes used are pyvista meshes."""
-    mesh.points -= np.array(mesh.center)[None, :]
-    diameter = 0.5 * (mesh.points.max() - mesh.points.min())
-    mesh.points *= scale / diameter
-    return mesh
