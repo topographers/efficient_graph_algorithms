85e0e9a98eee3a43dbd04aab986124a152bffae0
Arijit Sehanobish <arijitthegame@gmail.com>
2022-09-16 14:36:57 -0400

    add types

--- src/ega/util/mesh_transformations.py
+++ src/ega/util/mesh_transformations.py
@@ -1,7 +1,7 @@
 import numpy as np
+import pyvista
 
-
-def rescale_mesh(mesh, scale):
+def rescale_mesh(mesh: pyvista.PolyData, scale: float):
     """Function to rescale the meshes. The meshes used are pyvista meshes."""
     mesh.points -= np.array(mesh.center)[None, :]
     diameter = 0.5 * (mesh.points.max() - mesh.points.min())
