707df5311b87cf8b6237d853659735ebc30eebb9
Han Lin <hl.hanlin@hotmail.com>
2022-09-19 21:19:37 -0400

    Update __init__.py

--- src/ega/__init__.py
+++ src/ega/__init__.py
@@ -10,6 +10,7 @@ logger = pysimplelog.Logger()
 
 __current_path = os.path.dirname(__file__)
 default_curvox_dataset_path = os.path.abspath(os.path.join(__current_path, "../../", "data", "curvox_dataset"))
+default_meshgraphnet_dataset_path = os.path.abspath(os.path.join(__current_path, "../../", "data", "meshgraphnet"))
 default_training_path = os.path.abspath(os.path.join(__current_path, "../../", "data", "training"))
 root_path = os.path.dirname(os.path.realpath(__file__))
 
