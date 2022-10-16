2e45de2b838e23231f379b565fbf08811e29d2fd
Han Lin <hl.hanlin@hotmail.com>
2022-10-15 22:47:20 -0400

    deleted some unused import

--- src/ega/algorithms/lt_separation_finder.py
+++ src/ega/algorithms/lt_separation_finder.py
@@ -1,13 +1,7 @@
 import numpy as np
 from ega.algorithms.separation_finder import SeparationFinder 
-
-from planar_separators.lipton_tarjan import PlanarGraphEdges 
-from planar_separators.lipton_tarjan import PlanarGraph 
-from planar_separators.lipton_tarjan import Triangulator
-from planar_separators.lipton_tarjan import PlanarGraphGenerator 
 from planar_separators.lipton_tarjan import PlanarSeparator 
 from planar_separators.lipton_tarjan import PlanarGraphConstructor
-from planar_separators.lipton_tarjan import SeparationClass
 
 
 
