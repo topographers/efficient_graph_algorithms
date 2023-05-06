74523598a07aae87258014312323ce462997e332
yzhao3685 <yz3685_2@instance-2.us-central1-a.c.upheld-object-346412.internal>
2023-05-06 16:27:54 +0000

    latest changes

---
+++ scripts/experiments/compare_psgf_bfgf_wass_barycenter.py
@@ -0,0 +1,155 @@
+import argparse
+import numpy as np
+import trimesh
+import os
+from ega.util.get_mesh_data import get_mesh_data
+from ega.util.blur_on_mesh import blur_on_mesh
+from ega.algorithms.convolutional_barycenter import ConvolutionalBarycenter
+from ega.visualization.mesh_visualization import simple3d_save_gif
+from ega.algorithms.separation_gf_integrator import SeparationGFIntegrator
+from ega.algorithms.brute_force import BFGFIntegrator
+from ega.util.separator_utils import base_compute_clustering
+from ega.algorithms.lt_separation_finder import LTSeparationFinder
+from ega.util.gaussian_kernel import GaussianKernel
+import ega.util.mesh_utils as mu
+from ega.evaluation.evaluator import Evaluator
+from memory_profiler import profile
+from line_profiler import LineProfiler
+from sklearn.metrics import mean_squared_error
+import networkx
+import time
+import pdb
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('convbarycenter', add_help=False)
+    parser.add_argument(
+        '--niter',
+        dest='niter',
+        type=int,
+        default=100,
+        help="""number of iterations"""
+        )
+    parser.add_argument(
+        '--tol',
+        dest='tol',
+        type=float,
+        default=0.01,
+        help="""stopping tolerance"""
+        )
+    parser.add_argument(
+        '--verb',
+        dest='verb',
+        type=int, default=1,
+        help="""if set to 1, print information at each iteration"""
+        )
+    parser.add_argument(
+        '--object_folder',
+        dest='object_folder',
+        type=str,
+        default=os.getcwd() + '/meshes',
+        help="""path for sample data."""
+        )
+    parser.add_argument(
+        '--output_folder',
+        dest='output_folder',
+        type=str,
+        default=os.getcwd() + '/gif_files',
+        help="""path for saving plots."""
+        )
+    return parser
+
+def main():
+    parser = argparse.ArgumentParser('TopoGrapher', parents=[get_args_parser()])
+    args = parser.parse_args()
+
+    obj_name = 'dice'
+    object_mesh_path = os.path.join(args.object_folder, obj_name + '.obj') 
+    mesh = trimesh.load(object_mesh_path)
+    blur_time = .2 # if this gets too small, distances get noisy
+    blur_steps = 2
+    
+    mesh_dictionary = get_mesh_data(mesh.vertices, mesh.faces, blur_time, blur_steps, sparse=False)
+    graph_field_integrator_solomon_2015 = lambda x: blur_on_mesh(x, mesh_dictionary, blur_time, blur_steps)
+
+    # Design a few functions to average
+    if obj_name == 'dice':
+        center_verts = [1000, 2500, 4000]
+    elif obj_name == 'duck':
+        center_verts = [1000, 5000, 6000]
+    elif obj_name == 'land':
+        center_verts = [1000, 5000, 10000]
+    elif obj_name == 'bubblepot2':
+        center_verts = [1000, 8000, 15000]
+    elif obj_name == 'moomoo_s0':
+        center_verts = [100, 300, 600]
+    elif obj_name == 'octocat-v1':
+        center_verts = [1000, 8000, 100]
+    n_functions = len(center_verts)
+    distributions = np.zeros((mesh_dictionary['num_vertices'], n_functions))
+    for i in range(n_functions):
+        distributions[center_verts[i] - 1,i] = 1 / mesh_dictionary['area_weights'][center_verts[i] - 1]
+        distributions[:,i] =  blur_on_mesh(distributions[:,i], mesh_dictionary)
+    alpha = np.ones(3)
+
+    # create adjacency lists and weights lists
+    adjacency_lists = mu.trimesh_to_adjacency_matrices(mesh, seed=0)  
+    sigma = 0.2
+    kernel_function = GaussianKernel(sigma)
+    vertices = np.arange(mesh_dictionary['num_vertices'])
+    weights_lists = []
+    for i in range(len(adjacency_lists)):
+        weights_lists.append([1 for i in adjacency_lists[i]])
+
+    # brute force integrator initialization
+    start_time = time.time()
+    bf_integrator = BFGFIntegrator(adjacency_lists, weights_lists, vertices, kernel_function)
+    end_time = time.time()
+    print('brute force preprocessing time: ', end_time - start_time)
+
+    # planer separator integrator initialization
+    start_time = time.time()
+    threshold_nb_vertices = 2000 # adjust threshold_nb_vertices for different meshes
+    if obj_name == 'moomoo_s0':
+        threshold_nb_vertices = 800
+    elif obj_name == 'octocat-v1':
+        threshold_nb_vertices = 4000
+    psgf_integrator = SeparationGFIntegrator(adjacency_lists, weights_lists, vertices, kernel_function,
+                      separation_finder=LTSeparationFinder(), compute_clustering=base_compute_clustering,
+                      unit_size=0.1, laplace=True, threshold_nb_vertices=threshold_nb_vertices)
+    end_time = time.time()
+    print('planer separator preprocessing time: ', end_time - start_time)
+
+    # compare brute force integrator with planer separation integrator on wass barycenter task
+    conv_barycenter = ConvolutionalBarycenter(niter = args.niter, tol = args.tol, verb=args.verb, normalization=True)
+    start_time = time.time()
+    barycenter_brute_force = conv_barycenter.get_convolutional_barycenter(distributions, alpha,
+                     bf_integrator.integrate_graph_field, mesh_dictionary['area_weights'])
+    end_time = time.time()
+    print('brute force compute time: ', end_time - start_time)
+
+    start_time = time.time()
+    barycenter_psgf = conv_barycenter.get_convolutional_barycenter(distributions, alpha,
+                     psgf_integrator.integrate_graph_field, mesh_dictionary['area_weights'])
+    end_time = time.time()
+    print('planer separator compute time: ', end_time - start_time)
+
+    print('Wasserstein Barycenter, brute force: ')
+    print(barycenter_brute_force)
+    print('Wasserstein Barycenter, planer separator: ')
+    print(barycenter_psgf)
+
+    print('# vtcs ', mesh_dictionary['num_vertices'])
+    print('mean squared error', mean_squared_error(barycenter_brute_force, barycenter_psgf))
+    
+    # plot wasserstein barycentern and different input distributions
+    output_file_path = args.output_folder + "/bfgf_wass_barycenter" + ".gif"
+    simple3d_save_gif(barycenter_brute_force, mesh, output_file_path)    
+    output_file_path = args.output_folder + "/psgf_wass_barycenter" + ".gif"
+    simple3d_save_gif(barycenter_psgf, mesh, output_file_path)    
+    for i in range(n_functions):
+        output_file_path = args.output_folder + "/input_distribution_" + str(i + 1) + ".gif"
+        simple3d_save_gif(distributions[:,i], mesh, output_file_path)
+
+
+if __name__ == '__main__':
+    main()
--- src/ega/util/blur_on_mesh.py
+++ src/ega/util/blur_on_mesh.py
@@ -1,29 +1,33 @@
 import numpy as np
 import scipy
+from sksparse.cholmod import cholesky
 
-from ega.data_structures.mesh_data import MeshData
-
-
-def blur_on_mesh(signal: float, mesh_data: MeshData, time: float, steps: int):
-    """
+def blur_on_mesh(signal: float, mesh_dictionary: dict):
+    '''
     efficient matrix vector multiplication using cotangent Laplacian and Cholesky factorization.
     the procedure is described in (Solomon et al, 2015) section 5.
-
+    
     inputs:
         signal is the vector that we want to multiply by a matrix
         mesh_dictionary stores information of the mesh, including pre-computed cotangent Laplacian,
             which is a representation of the matrix
 
     output:
-        a 1d vector, the result of the matrix vector multplication
-    """
-    area_weights = mesh_data.area_weights
-    if steps <= 0:
-        raise ValueError('steps must be greater than zero')
-    matrix_to_pre_factor = np.diag(area_weights) + time / steps * mesh_data.cotangent_laplacian
-    # use cholesky factorization to efficiently solve systems of linear equations
-    L = np.linalg.cholesky(matrix_to_pre_factor)
+        a 1d vector, the result of the matrix vector multplication  
+    '''
     result = signal
-    for _ in range(steps):
-        result = scipy.linalg.cho_solve((L, True), np.multiply(result.T, area_weights).T)
+    area_weights = mesh_dictionary['area_weights']
+    if mesh_dictionary['sparse']:
+        factor = mesh_dictionary['factor']
+        for _ in range(mesh_dictionary['steps']):
+            b = np.multiply(result.T, area_weights).T
+            result = factor(b)
+    else:
+        L = mesh_dictionary['L']
+        for _ in range(mesh_dictionary['steps']):
+            b = np.multiply(result.T, area_weights).T
+            result = scipy.linalg.cho_solve((L, True), b)
     return result
+
+
+
